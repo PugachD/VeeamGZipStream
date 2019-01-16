d5670ac7cb992bfbd39b4fb966ae1c0b0f962bd7
Dmitriy <pugachev15@gmail.com>
2019-01-16 03:50:19 +0300

    Rename project

--- VeeamGZipStream.Test/Concurrency/ProducerConsumerTests.cs
+++ VeeamGZipStream.Test/Concurrency/ProducerConsumerTests.cs
@@ -1,10 +1,5 @@
 ﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
-using VeeamGZipStream.Concurrency;
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using VeeamGZipStream.Models;
 
 namespace VeeamGZipStream.Concurrency.Tests
